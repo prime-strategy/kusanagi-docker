dbbdb060a33592d64c8349fea896e99b40fb180d
Satoru MIYAZAKI <s.miyaza@gmail.com>
2019-02-05 14:30:14 +0900

    - modify filename TYPO.

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ export KUSANAGIDIR=$HOME/.kusanagi
 mkdir -p $KUSANAGIDIR && cd $KUSANAGIDIR
 git clone $@ https://github.com/prime-strategy/kusanagi-docker.git $KUSANGIDIR
 KUSANAGILIBDIR=$KUSANAGIDIR/lib
-source ./update_version/sh
+source ./update_version.sh
 
 for r in $(cat lib/.requires) ; do
 	which $r 2>&1 > /dev/null \
