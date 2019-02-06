78347c0a145e9f39cbcbb46af223eb6a84fbc171
Satoru MIYAZAKI <s.miyaza@gmail.com>
2019-02-06 13:18:04 +0900

    - TYPO(latast -> latest)

--- update_version.sh
+++ update_version.sh
@@ -3,27 +3,27 @@
 function kusanagi_version {
 	local _kusanagi=$1
 	local _ver=$(git ls-remote -t https://github.com/prime-strategy/$_kusanagi.git 2>&1 |awk '{print $2}' | awk -F/ '/^[0-9\.\-a-zA-Z\/]+$/ {print $NF}'|sort -rV | head -1)
-	echo ${_ver:-latast}
+	echo ${_ver:-latest}
 }
 
 function mariadb_version {
 	local _ver=$(curl -L https://raw.githubusercontent.com/docker-library/mariadb/master/10.4/Dockerfile 2> /dev/null  | awk -F'[ +~:]' '/ENV +MARIADB_VERSION/ {printf "%s-%s\n",$4,$6}')
-	echo ${_ver:-latast}
+	echo ${_ver:-latest}
 }
 
 function postgresql_version {
 	local _ver=$(curl -L  https://raw.githubusercontent.com/docker-library/postgres/master/11/alpine/Dockerfile 2> /dev/null | awk '/ENV +PG_VERSION/ {print $NF"-alpine"}')
-	echo ${_ver:-latast}
+	echo ${_ver:-latest}
 }
 
 function wpcli_version {
 	local _ver=$(curl -L https://raw.githubusercontent.com/docker-library/wordpress/master/php7.3/cli/Dockerfile 2> /dev/null | awk '/ENV +WORDPRESS_CLI_VERSION/ {print $NF}')
-	echo ${_ver:-latast}
+	echo ${_ver:-latest}
 }
 
 function certbot_version {
 	local _ver=$(git ls-remote -t https://github.com/certbot/certbot.git |awk '{print $2}' | awk -F/ '/^[0-9\.\-a-zA-Z\/]+$/ {print $NF}'|sort -Vr | head -1)
-	echo ${_ver:-latast}
+	echo ${_ver:-latest}
 }
 
 KUSANAGI_NGINX_IMAGE=primestrategy/kusanagi-nginx:$(kusanagi_version kusanagi-nginx)
